c4a96e7e262ba1e7ea08d69a57cdf182e3946706
Anastasia Balagurova <73748770+balagurova@users.noreply.github.com>
2024-10-31 23:47:01 +0200

    Add bar charts

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,10 @@
-import undpLogo from './assets/undp-logo-blue.svg';
+import { SingleGraphDashboardFromConfig } from '@undp-data/undp-visualization-library';
+import '@undp-data/undp-visualization-library/dist/style.css';
 
 function App() {
   return (
-    <div className='undp-container flex-div flex-wrap flex-hor-align-center margin-top-13 margin-bottom-13'>
-      <div>
-        <img
-          src={undpLogo}
-          className='logo react'
-          alt='React logo'
-          width='72px'
-          style={{ margin: 'auto' }}
-        />
-      </div>
-      <h3
-        className='undp-typography'
-        style={{ textAlign: 'center', width: '100%' }}
-      >
-        This is template for building visualization and frontend project
-        maintained by UNDP&apos;s SDG Integration Team.
-        <br />
-        <br />
-        Contact us at data@undp.org if you have any feedback or questions.
-      </h3>
+    <div>
+      <SingleGraphDashboardFromConfig config='/config/verticalGroupedBarChart.json' />
     </div>
   );
 }
