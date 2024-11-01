8f6f55ea3d0c1bf580f5d4e20bb4f24131e08402
Anastasia Balagurova <73748770+balagurova@users.noreply.github.com>
2024-11-01 18:48:20 +0200

    Update examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
-import {
-  GriddedGraphsFromConfig,
-  MultiGraphDashboardFromConfig,
-  SingleGraphDashboardFromConfig,
-} from '@undp-data/undp-visualization-library';
+// import { GriddedGraphsFromConfig } from '@undp-data/undp-visualization-library';
+// import { SingleGraphDashboardFromConfig } from '@undp-data/undp-visualization-library';
+// import { MultiGraphDashboardFromConfig } from '@undp-data/undp-visualization-library';
 import '@undp-data/undp-visualization-library/dist/style.css';
 
 function App() {
   return (
-    <div>
-      <MultiGraphDashboardFromConfig config='/config/dashboard.json' />
-      <GriddedGraphsFromConfig config='/config/griddedChart.json' />
-      <SingleGraphDashboardFromConfig config='/config/lineChart.json' />
+    <div className='flex-div flex-column'>
+      {/* Single Charts */}
+      {/* <SingleGraphDashboardFromConfig config='/config/charts/barChart.json' />
+      <SingleGraphDashboardFromConfig config='/config/charts/groupedBarChart.json' />
+      <SingleGraphDashboardFromConfig config='/config/charts/stackedBarChart.json' />
+      <SingleGraphDashboardFromConfig config='/config/charts/lineChart.json' />
+      <SingleGraphDashboardFromConfig config='/config/charts/multiLineChart.json' />
+      <SingleGraphDashboardFromConfig config='/config/charts/choroplethMap.json' />
+      <SingleGraphDashboardFromConfig config='/config/charts/donutChart.json' />
+      <SingleGraphDashboardFromConfig config='/config/charts/circlePacking.json' />
+      <SingleGraphDashboardFromConfig config='/config/charts/scatterPlot.json' /> */}
+
+      {/* Animated Single Charts */}
+      {/* <SingleGraphDashboardFromConfig config='/config/chartsAnimated/animatedBarChart.json' /> */}
+
+      {/* Single Chart with Filter */}
+
+      {/* Single Chart with Data Selection */}
+
+      {/* Gridded Chart (Small Multiples) */}
+      {/* <GriddedGraphsFromConfig config='/config/chartsGridded/griddedChart.json' /> */}
+
+      {/* Dashboard */}
+      {/* <MultiGraphDashboardFromConfig config='/config/dashboards/dashboard.json' /> */}
     </div>
   );
 }
