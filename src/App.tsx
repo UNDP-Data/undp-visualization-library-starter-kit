30b7d6fee795b50b3f420a38917f211369b7acf8
Anastasia Balagurova <73748770+balagurova@users.noreply.github.com>
2024-11-03 16:33:01 +0200

    Update examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // import { GriddedGraphsFromConfig } from '@undp-data/undp-visualization-library';
 // import { SingleGraphDashboardFromConfig } from '@undp-data/undp-visualization-library';
-// import { MultiGraphDashboardFromConfig } from '@undp-data/undp-visualization-library';
+import { MultiGraphDashboardFromConfig } from '@undp-data/undp-visualization-library';
 import '@undp-data/undp-visualization-library/dist/style.css';
 
 function App() {
@@ -18,17 +18,21 @@ function App() {
       <SingleGraphDashboardFromConfig config='/config/charts/scatterPlot.json' /> */}
 
       {/* Animated Single Charts */}
-      {/* <SingleGraphDashboardFromConfig config='/config/chartsAnimated/animatedBarChart.json' /> */}
+      {/* <SingleGraphDashboardFromConfig config='/config/chartsAnimated/animatedBarChart.json' />
+      <SingleGraphDashboardFromConfig config='/config/chartsAnimated/animatedScatterPlot.json' />
+      <SingleGraphDashboardFromConfig config='/config/chartsAnimated/animatedChoroplethMap.json' /> */}
 
       {/* Single Chart with Filter */}
+      {/* <SingleGraphDashboardFromConfig config='/config/charts/barChartWithFilter.json' /> */}
 
       {/* Single Chart with Data Selection */}
+      {/* <SingleGraphDashboardFromConfig config='/config/charts/barChartWithDataSelection.json' /> */}
 
       {/* Gridded Chart (Small Multiples) */}
       {/* <GriddedGraphsFromConfig config='/config/chartsGridded/griddedChart.json' /> */}
 
       {/* Dashboard */}
-      {/* <MultiGraphDashboardFromConfig config='/config/dashboards/dashboard.json' /> */}
+      <MultiGraphDashboardFromConfig config='/config/dashboards/dashboard.json' />
     </div>
   );
 }
